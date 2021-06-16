fc9cf204eb3d9344b9b420475d0db5a4188553d2
koenweverink <47668072+koenweverink@users.noreply.github.com>
2021-06-16 20:50:30 +0200

    Add files via upload

---
+++ velociraptor/main.py
@@ -0,0 +1,107 @@
+'''
+To start frontend:
+velociraptor.exe --config server.config.yaml frontend -v
+
+To add new user:
+velociraptor.exe --config server.config.yaml user add Koen --role administrator
+
+To add client:
+velociraptor.exe --config client.config.yaml client -v
+
+
+REFERENCE QUERIES:
+query_basic_info="""
+SELECT config.Version.Name AS Name,
+       config.Version.BuildTime as BuildTime,
+       config.Labels AS Labels,
+       Hostname, OS, Architecture,
+       Platform, PlatformVersion, KernelVersion, Fqdn,
+       {
+          SELECT * FROM if(
+             condition=version(plugin='wmi') != NULL,
+             then={
+                 SELECT Domain FROM wmi(query='SELECT Domain FROM win32_computersystem')
+             })
+       } AS ADDomain
+FROM info()
+"""
+
+query_users="""
+SELECT Name, Description, Mtime AS LastLogin
+FROM Artifact.Windows.Sys.Users()
+"""
+
+query_eventlog="""
+SELECT System.EventID.Value AS EventID,
+        System.TimeCreated.SystemTime AS TimeCreated,
+        System.Computer AS Computer,
+        EventData.LogonType As LogonType
+        
+FROM parse_evtx(filename='C:/Windows/System32/Winevt/Logs/Security.evtx') 
+WHERE System.EventID.Value = 4624 AND EventData.LogonType = 5
+"""
+'''
+
+from pyvelociraptor import api_pb2
+from pyvelociraptor import api_pb2_grpc
+
+import yaml
+import grpc
+import json
+import os 
+
+
+class Velociraptor:
+    '''
+    This script connects to the Velociraptor API through gRPC and takes VQL queries as variables.
+    '''
+    def __init__(self):
+        '''
+        Sets all the necessary configurations.
+        '''
+        super().__init__()
+        config_path = os.path.dirname(os.path.abspath(__file__))
+        filename = os.path.join(config_path, 'api_client.yaml')
+
+        self.config = yaml.safe_load(open(filename).read())
+        self.creds = grpc.ssl_channel_credentials(
+                root_certificates=self.config["ca_certificate"].encode("utf8"),
+                private_key=self.config["client_private_key"].encode("utf8"),
+                certificate_chain=self.config["client_cert"].encode("utf8"))
+
+        self.options = (('grpc.ssl_target_name_override', "VelociraptorServer",),)
+
+
+    def query(self, query, destination):
+        '''
+        Connects and calls the Velociraptor API and performs the defined query.
+        :param query: The VQL query
+        :type query: str
+        :param destination: The destination folder
+        :type destination: str 
+        '''
+        with grpc.secure_channel(self.config["api_connection_string"], self.creds, self.options) as channel:
+                stub = api_pb2_grpc.APIStub(channel)
+                
+                request = api_pb2.VQLCollectorArgs(Query=[api_pb2.VQLRequest(VQL=query,)])
+                
+                for response in stub.Query(request):
+                    if response.Response:
+                        for row in json.loads(response.Response):
+                            print(row)
+                            with open(destination, 'a', encoding='utf-8') as f:
+                                json.dump(row, f, ensure_ascii=False, indent=4)
+
+
+# if __name__ == "__main__":
+#     v = Velociraptor()
+
+#     dest = 'test_amc_p.json'
+#     query = """
+#         SELECT *
+#         FROM Artifact.Windows.System.Amcache()
+#         WHERE Name = 'format_sd.exe'
+#         OR Name = 'fold.exe'
+#         """
+#     v.query(query, dest)
+
