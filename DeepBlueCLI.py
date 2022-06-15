3709e9c5fd51294d35bea312d1349371819deb17
koenweverink <47668072+koenweverink@users.noreply.github.com>
2022-06-15 15:31:46 +0200

    Update DeepBlueCLI.py

--- DeepBlueCLI.py
+++ DeepBlueCLI.py
@@ -5,5 +5,5 @@ class DeepBlueCLI:
     '''Using DeepBlueCLI triaging'''
     def deepBlue(self):
         '''Using subprocess.check_output to start powershell.exe and run DeepBlue.ps1'''
-        output = check_output(["powershell.exe", "C:\\Users\koenw\Desktop\School\Afstuderen\DeepBlueCLI\DeepBlueCLI\DeepBlue.ps1"])
+        output = check_output(["powershell.exe", "C:\\Users\...\DeepBlueCLI\DeepBlueCLI\DeepBlue.ps1"])
         return output
